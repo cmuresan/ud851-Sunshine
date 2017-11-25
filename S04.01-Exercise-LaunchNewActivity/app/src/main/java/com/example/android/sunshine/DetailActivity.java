f1485ede8dc110f2028f5daa7e744b1e4536eea4
cristianmmuresan <cristian@wearewip.com>
2017-11-25 21:36:15 +0200

    S04.01 - LaunchNewActivity

---
+++ S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -0,0 +1,9 @@
+package com.example.android.sunshine;
+
+import android.support.v7.app.AppCompatActivity;
+/**
+ * Created by Cristian on 11/25/2017.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+}
